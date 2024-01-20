2d993869a3f5237825ad6f94dba41c61021920e3
Kai Kang <cctv2206@outlook.com>
2024-01-20 14:31:57 +0800

    MOD: update user profile

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,39 +1,78 @@
 import { Pool } from "pg";
 import { Request, Response, NextFunction } from 'express';
 import { User } from "../model/user";
+import { doInTxn } from "../util/db.util";
 
 export const apiUpdateUserProfile = (pool: Pool) => {
   return async (req: Request, res: Response) => {
-    const { name, language } = req.body;
-    
-    let user = req.user as User;
+    const { name } = req.body;
 
-    // todo: username should be unique
-    if (name) {
-      user.username = name;
+    if (!name) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Name is required.',
+      });
+      return;
     }
 
-    if (language) {
-      user.language = language;
+    const user = req.user as User;
+
+    if (name === user.username) {
+      res.json({
+        status: 'success',
+        data: {
+          username: user.username
+        },
+        message: `User profile updated successfully.`,
+      });
+      return;
     }
 
-    const updateQuery = `
-      UPDATE users
-      SET username = $1, language = $2
-      WHERE id = $3
-      returning *
-    `;
-    const values = [user.username, user.language, user.id];
-    const result = await pool.query(updateQuery, values);
-    user = result.rows[0];
+    const sameNameRows = await doInTxn(pool, async (client) => {
+      const query = `
+        SELECT *
+        FROM users
+        WHERE username = $1
+      `;
+      const values = [name];
+      const result = await client.query(query, values);
+      return result.rows;
+    });
+
+    if (sameNameRows.length > 0) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Name is already taken. Kindly ask user to choose another name.',
+      });
+      return;
+    }
+
+    const rows = await doInTxn(pool, async (client) => {
+      const updateQuery = `
+        UPDATE users
+        SET username = $1
+        WHERE id = $2
+        returning *
+      `;
+      const values = [name, user.id];
+      const result = await pool.query(updateQuery, values);
+      return result.rows;
+    })
+
+    if (!rows.length) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Update user profile error. Please try again later.',
+      });
+      return;
+    }
 
     res.json({
       status: 'success',
       data: {
-        username: user.username,
-        language: user.language,
+        username: name
       },
-      message: `User profile updated successfully. From now on, remember to user the new username and user's preferred language`,
-  });
+      message: `User profile updated successfully.`,
+    });
   }
 }
